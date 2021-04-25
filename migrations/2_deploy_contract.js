8a843219488d0d4bd1af56712e52dd63a1a35b8b
pavanvsoratur <pavanvsoratur@gmail.com>
2021-04-26 01:22:02 +0530

    removes truffle config file, migrations folder and contracts folder in client/src

--- migrations/2_deploy_contract.js
+++
@@ -1,8 +0,0 @@
-const AutoAppealableArbitrator = artifacts.require("../client/src/contracts/AutoAppealableArbitrator");
-const SafexMain = artifacts.require("../client/src/contracts/SafexMain");
-
-module.exports = function (deployer) {
-  deployer.deploy(AutoAppealableArbitrator, 1000000000000000).then((res) => {
-    return deployer.deploy(SafexMain, res.address);
-  });
-};
