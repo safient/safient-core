fdc34f6e606e20db015932689d4bb2adf10b6bea
pavanvsoratur <pavanvsoratur@gmail.com>
2021-04-27 16:13:36 +0530

    incorporates the review comment change requests

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,34 +1,34 @@
+require("dotenv").config();
+
 const { utils, ethers } = require("ethers");
 const fs = require("fs");
 const chalk = require("chalk");
 
 require("@nomiclabs/hardhat-waffle");
 
-const defaultNetwork = "localhost"; // "hardhat" for tests
-const INFURA_API_KEY = "ffffffffffffffffffffffffffffffff";
-const TESTNET_ACCOUNT_PRIVATE_KEY = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+const defaultNetwork = "localhost";
 
 module.exports = {
   defaultNetwork,
   networks: {
     localhost: {
-      url: "http://localhost:8545", // uses account 0 of the hardhat node to deploy
+      url: "http://localhost:8545",
     },
     mainnet: {
-      url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [`0x${TESTNET_ACCOUNT_PRIVATE_KEY}`],
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.TESTNET_ACCOUNT_PRIVATE_KEY}`],
     },
     rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [`0x${TESTNET_ACCOUNT_PRIVATE_KEY}`],
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.TESTNET_ACCOUNT_PRIVATE_KEY}`],
     },
     kovan: {
-      url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [`0x${TESTNET_ACCOUNT_PRIVATE_KEY}`],
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.TESTNET_ACCOUNT_PRIVATE_KEY}`],
     },
     ropsten: {
-      url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [`0x${TESTNET_ACCOUNT_PRIVATE_KEY}`],
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.TESTNET_ACCOUNT_PRIVATE_KEY}`],
     },
   },
   solidity: {
@@ -53,26 +53,4 @@ module.exports = {
       },
     ],
   },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts",
-  },
 };
-
-task("accounts", "Prints the list of accounts", async () => {
-  if (defaultNetwork === "localhost") {
-    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/");
-    const accounts = await provider.listAccounts();
-    for (let i = 0; i < accounts.length; i++) {
-      const accountBalance = await provider.getBalance(accounts[i]);
-      console.log("📄", chalk.cyan(accounts[i]), "💸", chalk.magenta(utils.formatEther(accountBalance), "ETH"));
-    }
-    console.log("\n");
-  } else {
-    console.log(
-      " ⚠️  This task only runs on JsonRpcProvider running a node at " + chalk.magenta("localhost:8545") + "\n"
-    );
-  }
-});
--- test/SafexMain.js
+++ test/SafexMain.js
@@ -3,17 +3,16 @@ const { use, expect } = require("chai");
 const { solidity } = require("ethereum-waffle");
 
 use(solidity);
-use(require("chai-as-promised")).should();
 
 describe("SafexMain", async () => {
-  let autoAppealableArbitrator, safexMain, acc0, acc1, acc2, acc3, acc4, acc5, acc6, acc7, acc8, acc9;
+  let autoAppealableArbitrator, safexMain, safexMainAdminAndArbitrator, planCreator, inheritor, accountX;
 
   describe("SafexMain Flow", async () => {
     it("Should deploy SafexMain", async () => {
-      [acc0, acc1, acc2, acc3, acc4, acc5, acc6, acc7, acc8, acc9] = await ethers.getSigners();
+      [safexMainAdminAndArbitrator, planCreator, inheritor, accountX] = await ethers.getSigners();
 
       const AutoAppealableArbitrator = await ethers.getContractFactory("AutoAppealableArbitrator");
-      autoAppealableArbitrator = await AutoAppealableArbitrator.deploy(1000000000000000);
+      autoAppealableArbitrator = await AutoAppealableArbitrator.deploy(ethers.utils.parseEther("0.001"));
       await autoAppealableArbitrator.deployed();
 
       const SafexMain = await ethers.getContractFactory("SafexMain");
@@ -21,53 +20,48 @@ describe("SafexMain", async () => {
       await safexMain.deployed();
 
       expect(await safexMain.arbitrator()).to.equal(autoAppealableArbitrator.address);
-      expect(await autoAppealableArbitrator.arbitrationCost(123)).to.equal(1000000000000000);
+      expect(await autoAppealableArbitrator.arbitrationCost(123)).to.equal(ethers.utils.parseEther("0.001"));
     });
 
     it("Should allow users to create a plan", async () => {
-      expect(await safexMain.plansCount()).to.equal(0);
-      expect(await safexMain.getSafexMainContractBalance()).to.equal(0);
-
-      const arbitrationFee = await autoAppealableArbitrator.arbitrationCost(123); // 1000000000000000 (0.001 eth)
+      const arbitrationFee = await autoAppealableArbitrator.arbitrationCost(123); // 0.001 eth
 
       // SUCCESS : create a plan
       await safexMain
-        .connect(acc0)
+        .connect(planCreator)
         .createPlan(
-          acc9.address,
+          inheritor.address,
           "https://bafybeif52vrffdp7m2ip5f44ox552r7p477druj2w4g3r47wpuzdn7235y.ipfs.infura-ipfs.io/",
           {
-            value: arbitrationFee.toNumber() + 1000000000000000,
+            value: arbitrationFee.toNumber() + ethers.utils.parseEther("0.001").toNumber(),
           }
         );
 
       expect(await safexMain.plansCount()).to.equal(1);
-      expect(await safexMain.getSafexMainContractBalance()).to.equal(2000000000000000); // 0.002 eth
+      expect(await safexMain.getSafexMainContractBalance()).to.equal(ethers.utils.parseEther("0.002")); // 0.002 eth
 
       const plan = await safexMain.plans(1);
 
-      expect(plan.planCreatedBy).to.equal(acc0.address);
-      expect(plan.planCurrentOwner).to.equal(acc0.address);
-      expect(plan.metaEvidenceId).to.equal(1);
-      expect(plan.claimsCount).to.equal(0);
-      expect(plan.planFunds).to.equal(2000000000000000); // 0.002 eth
+      expect(plan.planCreatedBy).to.equal(planCreator.address);
+      expect(plan.planInheritor).to.equal(inheritor.address);
+      expect(plan.planFunds).to.equal(ethers.utils.parseEther("0.002")); // 0.002 eth
 
       // FAILURE : paying inadequate or no fee(arbitration fee) for plan creation
       await expect(
         safexMain
-          .connect(acc0)
+          .connect(planCreator)
           .createPlan(
-            acc9.address,
+            inheritor.address,
             "https://bafybeif52vrffdp7m2ip5f44ox552r7p477druj2w4g3r47wpuzdn7235y.ipfs.infura-ipfs.io/",
             {
-              value: arbitrationFee.toNumber() - 1000000000000000,
+              value: arbitrationFee.toNumber() - ethers.utils.parseEther("0.001").toNumber(),
             }
           )
       ).to.be.revertedWith("Inadequate fee payment");
 
       // FAILURE : metaEvidence is not passed
       await expect(
-        safexMain.connect(acc1).createPlan(acc8.address, "", {
+        safexMain.connect(planCreator).createPlan(inheritor.address, "", {
           value: arbitrationFee.toNumber(),
         })
       ).to.be.revertedWith("Should provide metaEvidence to create a plan");
@@ -75,7 +69,7 @@ describe("SafexMain", async () => {
       // FAILURE : inheritor is an zero address
       await expect(
         safexMain
-          .connect(acc1)
+          .connect(planCreator)
           .createPlan(
             "0x0000000000000000000000000000000000000000",
             "https://bafybeif52vrffdp7m2ip5f44ox552r7p477druj2w4g3r47wpuzdn7235y.ipfs.infura-ipfs.io/",
@@ -88,9 +82,9 @@ describe("SafexMain", async () => {
       // FAILURE : plan creator and inheritor are same
       await expect(
         safexMain
-          .connect(acc1)
+          .connect(planCreator)
           .createPlan(
-            acc1.address,
+            planCreator.address,
             "https://bafybeif52vrffdp7m2ip5f44ox552r7p477druj2w4g3r47wpuzdn7235y.ipfs.infura-ipfs.io/",
             {
               value: arbitrationFee.toNumber(),
@@ -100,53 +94,45 @@ describe("SafexMain", async () => {
     });
 
     it("Should allow inheritors to create a claim", async () => {
-      expect(await safexMain.getSafexMainContractBalance()).to.equal(2000000000000000); // 0.002 eth
-
-      let plan;
-      plan = await safexMain.plans(1);
-
-      expect(plan.planFunds).to.equal(2000000000000000); // 0.002 eth
-
       // FAILURE : plan does not exist
       await expect(
         safexMain
-          .connect(acc9)
+          .connect(inheritor)
           .createClaim(4, "https://bafybeif52vrffdp7m2ip5f44ox552r7p477druj2w4g3r47wpuzdn7235y.ipfs.infura-ipfs.io/")
       ).to.be.revertedWith("Plan does not exist");
 
       // FAILURE : only inheritor of the plan can create the claim
       await expect(
         safexMain
-          .connect(acc8)
+          .connect(accountX)
           .createClaim(1, "https://bafybeif52vrffdp7m2ip5f44ox552r7p477druj2w4g3r47wpuzdn7235y.ipfs.infura-ipfs.io/")
       ).to.be.revertedWith("Only inheritor of the plan can create the claim");
 
       // SUCCESS : create 1st claim
       await safexMain
-        .connect(acc9)
+        .connect(inheritor)
         .createClaim(1, "https://bafybeif52vrffdp7m2ip5f44ox552r7p477druj2w4g3r47wpuzdn7235y.ipfs.infura-ipfs.io/"); // evidence
 
-      expect(await safexMain.getSafexMainContractBalance()).to.equal(1000000000000000); // 0.001 eth
+      expect(await safexMain.getSafexMainContractBalance()).to.equal(ethers.utils.parseEther("0.001")); // 0.001 eth
 
+      let plan;
       plan = await safexMain.plans(1);
 
-      expect(plan.planFunds).to.equal(1000000000000000); // 0.001 eth
+      expect(plan.planFunds).to.equal(ethers.utils.parseEther("0.001")); // 0.001 eth
       expect(plan.claimsCount).to.equal(1);
 
       const claim1 = await safexMain.claims(0);
 
-      expect(claim1.planId).to.equal(1);
       expect(claim1.disputeId).to.equal(0);
-      expect(claim1.claimedBy).to.equal(acc9.address);
-      expect(claim1.metaEvidenceId).to.equal(1);
-      expect(claim1.evidenceGroupId).to.equal(1);
-      expect(claim1.status).to.equal(0); // Active
+      expect(claim1.claimedBy).to.equal(inheritor.address);
       expect(claim1.result).to.equal("Active");
 
       // SUCCESS : create 2nd claim on the same plan
       await safexMain
-        .connect(acc9)
+        .connect(inheritor)
         .createClaim(1, "https://bafybeif52vrffdp7m2ip5f44ox552r7p477druj2w4g3r47wpuzdn7235y.ipfs.infura-ipfs.io/"); // evidence
+
+      expect(await safexMain.getSafexMainContractBalance()).to.equal(0); // 0 eth
 
       plan = await safexMain.plans(1);
 
@@ -155,99 +141,91 @@ describe("SafexMain", async () => {
 
       const claim2 = await safexMain.claims(1);
 
-      expect(claim2.planId).to.equal(1);
       expect(claim2.disputeId).to.equal(1);
-      expect(claim2.claimedBy).to.equal(acc9.address);
-      expect(claim2.metaEvidenceId).to.equal(1);
-      expect(claim2.evidenceGroupId).to.equal(2);
-      expect(claim2.status).to.equal(0); // Active
+      expect(claim2.claimedBy).to.equal(inheritor.address);
       expect(claim2.result).to.equal("Active");
 
       // FAILURE : total number of claims on a plan has reached the limit
       await expect(
         safexMain
-          .connect(acc9)
+          .connect(inheritor)
           .createClaim(1, "https://bafybeif52vrffdp7m2ip5f44ox552r7p477druj2w4g3r47wpuzdn7235y.ipfs.infura-ipfs.io/")
       ).to.be.revertedWith("Total number of claims on a plan has reached the limit");
 
       // FAILURE : insufficient funds in the plan to pay the arbitration fee
       await expect(
         safexMain
-          .connect(acc9)
+          .connect(inheritor)
           .createClaim(1, "https://bafybeif52vrffdp7m2ip5f44ox552r7p477druj2w4g3r47wpuzdn7235y.ipfs.infura-ipfs.io/")
       ).to.be.reverted;
     });
 
     it("Should allow arbitrator to give ruling on a claim", async () => {
       // FAILURE : invalid ruling (only 2 options are available, but giving 3rd option as a ruling is invalid) - as per autoAppealableArbitrator
-      await expect(autoAppealableArbitrator.connect(acc0).giveRuling(0, 3)).to.be.revertedWith("Invalid ruling");
+      await expect(autoAppealableArbitrator.connect(safexMainAdminAndArbitrator).giveRuling(0, 3)).to.be.revertedWith(
+        "Invalid ruling"
+      );
 
       // FAILURE : can only be called by the owner - as per autoAppealableArbitrator
-      await expect(autoAppealableArbitrator.connect(acc5).giveRuling(0, 2)).to.be.revertedWith(
+      await expect(autoAppealableArbitrator.connect(accountX).giveRuling(0, 2)).to.be.revertedWith(
         "Can only be called by the owner"
       );
 
       // SUCCESS : give a ruling to claim1
-      await autoAppealableArbitrator.connect(acc0).giveRuling(0, 2);
+      await autoAppealableArbitrator.connect(safexMainAdminAndArbitrator).giveRuling(0, 2);
 
       const claim1 = await safexMain.claims(0);
 
-      expect(claim1.status).to.equal(2); // Failed
       expect(claim1.result).to.equal("Failed"); // Failed
 
       // SUCCESS : give a ruling to claim2
-      await autoAppealableArbitrator.connect(acc0).giveRuling(1, 0);
+      await autoAppealableArbitrator.connect(safexMainAdminAndArbitrator).giveRuling(1, 0);
 
       const claim2 = await safexMain.claims(1);
 
-      expect(claim2.status).to.equal(3); // Refused
-      expect(claim2.result).to.equal("RTA"); // RTA
+      expect(claim2.result).to.equal("RTA"); // Refused To Arbitrate (RTA)
     });
 
     it("Should allow users to deposit funds in a plan", async () => {
-      expect(await safexMain.getSafexMainContractBalance()).to.equal(0); // 0 eth
-
       // FAILURE : plan does not exist
       await expect(
-        safexMain.connect(acc4).depositPlanFunds(4, { value: ethers.utils.parseEther("2") }) // 2 eth
+        safexMain.connect(accountX).depositPlanFunds(4, { value: ethers.utils.parseEther("2") }) // 2 eth
       ).to.be.revertedWith("Plan does not exist");
 
       // SUCCESS : deposit funds in a plan
-      await safexMain.connect(acc4).depositPlanFunds(1, { value: ethers.utils.parseEther("2") }); // 2 eth
+      await safexMain.connect(accountX).depositPlanFunds(1, { value: ethers.utils.parseEther("2") }); // 2 eth
 
       expect(await safexMain.getSafexMainContractBalance()).to.equal(ethers.utils.parseEther("2")); // 2 eth
     });
 
     it("Should allow the plan owner to recover funds in the plan", async () => {
-      expect(await safexMain.getSafexMainContractBalance()).to.equal(ethers.utils.parseEther("2")); // 2 eth
-
       // FAILURE : plan does not exist
-      await expect(safexMain.connect(acc0).recoverPlanFunds(4)).to.be.revertedWith("Plan does not exist");
+      await expect(safexMain.connect(planCreator).recoverPlanFunds(4)).to.be.revertedWith("Plan does not exist");
 
       // FAILURE : only plan owner can recover the funds
-      await expect(safexMain.connect(acc3).recoverPlanFunds(1)).to.be.revertedWith(
+      await expect(safexMain.connect(accountX).recoverPlanFunds(1)).to.be.revertedWith(
         "Only plan owner can recover the deposit balance"
       );
 
       // SUCCESS : recover funds from a plan
-      await safexMain.connect(acc0).recoverPlanFunds(1);
+      await safexMain.connect(planCreator).recoverPlanFunds(1);
 
-      expect(await safexMain.getSafexMainContractBalance()).to.equal(0);
+      expect(await safexMain.getSafexMainContractBalance()).to.equal(0); // 0 eth
 
       // FAILURE : no funds remaining in the plan
-      await expect(safexMain.connect(acc0).recoverPlanFunds(1)).to.be.revertedWith("No funds remaining in the plan");
+      await expect(safexMain.connect(planCreator).recoverPlanFunds(1)).to.be.revertedWith(
+        "No funds remaining in the plan"
+      );
     });
 
     it("Should allow it's admin to set the total number of claims allowed on a plan", async () => {
-      expect(await safexMain.getTotalClaimsAllowed()).to.equal(2);
-
       // FAILURE : only SafexMain contract's admin can execute this
-      await expect(safexMain.connect(acc4).setTotalClaimsAllowed(3)).to.be.revertedWith(
+      await expect(safexMain.connect(accountX).setTotalClaimsAllowed(3)).to.be.revertedWith(
         "Only SafexMain contract's admin can execute this"
       );
 
       // SUCCESS : set new total number of claims allowed
-      await safexMain.connect(acc0).setTotalClaimsAllowed(4);
+      await safexMain.connect(safexMainAdminAndArbitrator).setTotalClaimsAllowed(4);
 
       expect(await safexMain.getTotalClaimsAllowed()).to.equal(4);
     });
